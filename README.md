15f0536f471e811519d82ca4f4a34ac99f137bcf
dokechin <ttatsumi@ra2.so-net.ne.jp>
2017-05-22 07:52:16 +0900

    commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+BigOne - It's new $module
+
+# SYNOPSIS
+
+    use BigOne;
+
+# DESCRIPTION
+
+BigOne is ...
+
+# LICENSE
+
+Copyright (C) dokechin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+dokechin <ttatsumi@ra2.so-net.ne.jp>
---
+++ lib/BigOne.pm
@@ -0,0 +1,39 @@
+package BigOne;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+BigOne - It's new $module
+
+=head1 SYNOPSIS
+
+    use BigOne;
+
+=head1 DESCRIPTION
+
+BigOne is ...
+
+=head1 LICENSE
+
+Copyright (C) dokechin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+dokechin E<lt>ttatsumi@ra2.so-net.ne.jpE<gt>
+
+=cut
+
